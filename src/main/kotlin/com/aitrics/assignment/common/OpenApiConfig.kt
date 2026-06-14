7d9c397bee7187ed33b49ccda75e072b5023b34e
bruce <bruce@bruceui-MacBookPro.local>
2026-06-14 20:05:58 +0900

    Swagger Authorize 버튼 활성화를 위한 설정 방식 변경

--- src/main/kotlin/com/aitrics/assignment/common/OpenApiConfig.kt
+++ src/main/kotlin/com/aitrics/assignment/common/OpenApiConfig.kt
@@ -1,12 +1,10 @@
 package com.aitrics.assignment.common
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
 import io.swagger.v3.oas.annotations.info.Info
-import io.swagger.v3.oas.models.Components
-import io.swagger.v3.oas.models.OpenAPI
-import io.swagger.v3.oas.models.security.SecurityRequirement
-import io.swagger.v3.oas.models.security.SecurityScheme
-import org.springframework.context.annotation.Bean
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import org.springframework.context.annotation.Configuration
 
 @Configuration
@@ -15,25 +13,13 @@ import org.springframework.context.annotation.Configuration
         title = "AITRICS Patient Monitoring API",
         version = "v1",
         description = "환자 생체징후 데이터 관리 및 위험도 추론 시스템 API 명세"
-    )
+    ),
+    security = [SecurityRequirement(name = "Bearer Auth")]
 )
-class OpenApiConfig {
-
-    @Bean
-    fun openAPI(): OpenAPI {
-        val securitySchemeName = "Bearer Auth"
-        val securityRequirement = SecurityRequirement().addList(securitySchemeName)
-        val components = Components().addSecuritySchemes(
-            securitySchemeName,
-            SecurityScheme()
-                .name(securitySchemeName)
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-        )
-
-        return OpenAPI()
-            .addSecurityItem(securityRequirement)
-            .setComponents(components)
-    }
-}
+@SecurityScheme(
+    name = "Bearer Auth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
+class OpenApiConfig
